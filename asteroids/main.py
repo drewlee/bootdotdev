8fdc5ff53fe80d5bfa54b12bca4ab03c5841861a
Andrew A Lee <andrew@andrewleedev.com>
2026-02-11 15:46:23 -0800

    Asteroids progress work

--- asteroids/main.py
+++ asteroids/main.py
@@ -10,14 +10,21 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+        log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-            log_state()
-            screen.fill("black")
-            pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000 
 
 
 if __name__ == "__main__":
