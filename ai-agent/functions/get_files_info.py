bd65afd52e5ff3795fc3047b662b12d4c5b20df1
Andrew A Lee <andrew@andrewleedev.com>
2026-02-28 10:01:56 -0800

    Adds get files function

---
+++ ai-agent/functions/get_files_info.py
@@ -0,0 +1,35 @@
+import os
+
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        if not os.path.isdir(target_dir):
+            return f'Error: "{target_dir}" is not a directory'
+
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+
+        if not valid_target_dir:
+            return (
+                f'Error: Cannot list "{directory}" as it is '
+                "outside the permitted working directory"
+            )
+
+        out_list = []
+        for item in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, item)
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            out_list.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+        out_list.sort()
+
+        msg_dir = "." if directory == "." else f'"{directory}"'
+        msg = f"Result for {msg_dir} directory:\n"
+        out = msg + "\n".join(out_list)
+
+        return out
+    except Exception as e:
+        return f"Error: {e}"
