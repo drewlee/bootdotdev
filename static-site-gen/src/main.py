4cabd19d827b293275d42f8ca95b2305eec653f3
Andrew A Lee <andrew@andrewleedev.com>
2026-03-08 17:15:10 -0700

    Adds more static site content

--- static-site-gen/src/main.py
+++ static-site-gen/src/main.py
@@ -1,12 +1,15 @@
 import os
 import shutil
 from os import path
+from pathlib import Path
 from md_to_html import markdown_to_html_node
 from extract_markdown import extract_title
 
 REL_PROJECT_ROOT = ".."
 PUBLIC_DIR = "public"
 STATIC_DIR = "static"
+CONTENT_DIR = "content"
+TMPL_FILE = "template.html"
 
 
 def get_abs_path(rel_path):
@@ -45,32 +48,49 @@ def copy_dir_contents(source, destination):
 
 
 def generate_page(from_path, template_path, dest_path):
-    abs_from_path = get_abs_path(from_path)
-    with open(abs_from_path, encoding="utf-8") as file:
+    with open(from_path, encoding="utf-8") as file:
         markdown = file.read()
 
-    abs_template_path = get_abs_path(template_path)
-    with open(abs_template_path, encoding="utf-8") as file:
+    with open(template_path, encoding="utf-8") as file:
         tmpl_html = file.read()
-
-    abs_dest_path = get_abs_path(dest_path)
 
     print(
         (
-            f'Generating page from "{abs_from_path}" to '
-            f'"{abs_dest_path}" using "{abs_template_path}"\n'
+            f'Generating page from "{from_path}" to '
+            f'"{dest_path}" using "{template_path}"\n'
         )
     )
 
+    doc_title = extract_title(markdown)
     html_node = markdown_to_html_node(markdown)
     html = html_node.to_html()
 
-    doc_title = extract_title(markdown)
     tmpl_html = tmpl_html.replace("{{ Title }}", doc_title)
     tmpl_html = tmpl_html.replace("{{ Content }}", html)
 
-    with open(path.join(abs_dest_path, "index.html"), "w", encoding="utf-8") as file:
+    dest_dirname = path.dirname(dest_path)
+    if not path.exists(dest_dirname):
+        os.makedirs(dest_dirname)
+
+    with open(dest_path, "w", encoding="utf-8") as file:
         file.write(tmpl_html)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    file_list = os.listdir(dir_path_content)
+    if not file_list:
+        print(f'No files found at "{dir_path_content}"\n')
+        return
+
+    for name in file_list:
+        file_path = path.join(dir_path_content, name)
+        if path.isdir(file_path):
+            generate_pages_recursive(
+                file_path, template_path, path.join(dest_dir_path, name)
+            )
+        elif Path(name).suffix == ".md":
+            destination = path.join(dest_dir_path, Path(name).stem + ".html")
+            generate_page(file_path, template_path, destination)
 
 
 def main():
@@ -80,7 +100,9 @@ def main():
     static_dir = get_abs_path(STATIC_DIR)
     copy_dir_contents(static_dir, public_dir)
 
-    generate_page("content/index.md", "template.html", PUBLIC_DIR)
+    content_dir = get_abs_path(CONTENT_DIR)
+    tmpl_file = get_abs_path(TMPL_FILE)
+    generate_pages_recursive(content_dir, tmpl_file, public_dir)
 
 
 if __name__ == "__main__":
