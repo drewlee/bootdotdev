bd0591855be7ae48d33b81dbd074f2b22807e034
Andrew A Lee <andrew@andrewleedev.com>
2026-07-07 09:51:16 -0700

    Adds health readiness endpoint

--- web-server/src/index.ts
+++ web-server/src/index.ts
@@ -1,9 +1,16 @@
-import express from 'express';
+import express, { type Request, type Response } from 'express';
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static('.'));
+function handlerReadiness(req: Request, res: Response): void {
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  res.send('OK');
+}
+
+app.use('/app', express.static('./src/app'));
+
+app.get('/healthz', handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
