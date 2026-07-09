3215ba9c16fe6344f9df82533ee434f6732c0711
Andrew A Lee <andrew@andrewleedev.com>
2026-07-09 11:18:25 -0700

    Adds metrics middleware

---
+++ web-server/src/api/middleware.ts
@@ -0,0 +1,24 @@
+import type { NextFunction, Request, Response } from 'express';
+import { config } from '../config.js';
+
+export function middlewareLogResponse(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  res.on('finish', () => {
+    if (res.statusCode >= 300) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+    }
+  });
+  next();
+}
+
+export function middlewareMetricsInc(
+  _: Request,
+  __: Response,
+  next: NextFunction,
+): void {
+  config.fileServerHits++;
+  next();
+}
--- web-server/src/index.ts
+++ web-server/src/index.ts
@@ -1,27 +1,31 @@
 import express, { type NextFunction, type Request, type Response } from 'express';
+import { middlewareLogResponse, middlewareMetricsInc } from './api/middleware.js';
+import { config } from './config.js';
 
 const app = express();
 const PORT = 8080;
-
-function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
-  res.on('finish', () => {
-    if (res.statusCode >= 400) {
-      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
-    }
-  });
-
-  next();
-}
 
 function handlerReadiness(req: Request, res: Response): void {
   res.set('Content-Type', 'text/plain; charset=utf-8');
   res.send('OK');
 }
 
-app.use(middlewareLogResponses);
-app.use('/app', express.static('./src/app'));
+function handlerMetrics(_: Request, res: Response): void {
+  res.send(`Hits: ${config.fileServerHits}`);
+}
+
+function handlerReset(_: Request, res: Response): void {
+  config.fileServerHits = 0;
+  res.write('Hits reset to 0');
+  res.end();
+}
+
+app.use(middlewareLogResponse);
+app.use('/app', middlewareMetricsInc, express.static('./src/app'));
 
 app.get('/healthz', handlerReadiness);
+app.get('/metrics', handlerMetrics);
+app.get('/reset', handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
