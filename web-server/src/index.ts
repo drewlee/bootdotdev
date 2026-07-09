d56b5c42403c327930460d0cb9944c8a4661fced
Andrew A Lee <andrew@andrewleedev.com>
2026-07-08 17:52:39 -0700

    Adds logging middleware

--- web-server/src/index.ts
+++ web-server/src/index.ts
@@ -1,13 +1,24 @@
-import express, { type Request, type Response } from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
 
 const app = express();
 const PORT = 8080;
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+  res.on('finish', () => {
+    if (res.statusCode >= 400) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+    }
+  });
+
+  next();
+}
 
 function handlerReadiness(req: Request, res: Response): void {
   res.set('Content-Type', 'text/plain; charset=utf-8');
   res.send('OK');
 }
 
+app.use(middlewareLogResponses);
 app.use('/app', express.static('./src/app'));
 
 app.get('/healthz', handlerReadiness);
