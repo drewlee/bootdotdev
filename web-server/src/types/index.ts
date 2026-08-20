219c6e302164fe0de83a51e394ad9d6b9afeaa84
Andrew A Lee <andrew@andrewleedev.com>
2026-08-20 15:54:25 -0700

    Removes web server project as it has been migrated to its own repo

--- web-server/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export const SORT_OPTIONS = ['asc', 'desc'] as const;
-
-export type SortOption = typeof SORT_OPTIONS[number];
--- web-server/vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    globals: true,
-    alias: [{ find: /^src\/(.*)/, replacement: './src/$1' }],
-  },
-});
